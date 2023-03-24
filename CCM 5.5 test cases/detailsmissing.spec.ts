479aedb9711ae6674fbbb0d6c4f3240401310d69
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2023-03-24 15:18:20 +0100

    24/03/2023

---
+++ CCM 5.5 test cases/detailsmissing.spec.ts
@@ -0,0 +1,14 @@
+//Client details field missing.
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/auth/login');
+  await page.getByRole('textbox', { name: 'username' }).fill('SUPERUSER');
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.locator('.css-mslw24').first().click();
+  await page.getByRole('tab', { name: 'Customer' }).click();
+  await expect(page.locator('text=Client details')).toHaveCount(1);
+});
---
+++ CCM 5.5 test cases/firstletter.spec.ts
@@ -0,0 +1,11 @@
+//First letter should be capital.
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/auth/login');
+  await page.getByRole('textbox', { name: 'username' }).fill('SUPERUSER');
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await expect(page.locator('text="Active"')).not.toHaveCount(0);
+});
