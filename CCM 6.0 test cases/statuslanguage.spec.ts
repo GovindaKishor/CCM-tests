30ea970f00a111dcf49f15d451ba4b62e03f20c9
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2023-03-30 16:52:41 +0200

    status language

--- CCM 6.0 test cases/statuslanguage.spec.ts
+++ CCM 6.0 test cases/statuslanguage.spec.ts
@@ -1,4 +1,4 @@
-//Status must be translated when I change user language.
+//Case status must be translated when I change user language.
 
 import { test, expect } from '@playwright/test';
 
@@ -12,27 +12,44 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Log in' }).click();
   await page.getByRole('tab', { name: 'Cases', exact: true }).click();
   await page.locator('div:nth-child(3) > .css-1ssirbu > .css-mslw24').click();
+  await page.getByRole('button', { name: 'Details' }).click();
   await expect(page.locator('text=Complete')).not.toHaveCount(0);
+  await expect(page.locator('text=Active')).not.toHaveCount(0);
+  await page.getByRole('link', { name: 'Clients' }).click();
+  await page.locator('div:nth-child(2) > .css-1ssirbu > .css-mslw24 > .MuiButtonBase-root > .PrivateSwitchBase-input').click();
+  await page.getByRole('button', { name: 'Related cases' }).click();
   await expect(page.locator('text=Active')).not.toHaveCount(0);
   await page.getByRole('button', { name: 'SS', exact: true }).click();
   await page.getByRole('menuitem', { name: 'Settings' }).click();
   await page.getByRole('button', { name: 'English (US)' }).click();
   await page.getByRole('option', { name: 'Deutsch' }).click();
   await page.getByRole('link', { name: 'Cases' }).click();
+  await page.locator('div:nth-child(3) > .css-1ssirbu > .css-mslw24').click();
   await expect(page.locator('text=Vollständig')).not.toHaveCount(0);
+  await expect(page.locator('text=Aktiv')).not.toHaveCount(0);
+  await page.getByRole('link', { name: 'Kunden' }).click();
+  await page.locator('div:nth-child(2) > .css-1ssirbu > .css-mslw24 > .MuiButtonBase-root > .PrivateSwitchBase-input').click();
   await expect(page.locator('text=Aktiv')).not.toHaveCount(0);
   await page.getByRole('button', { name: 'SS', exact: true }).click();
   await page.getByRole('menuitem', { name: 'Einstellungen' }).click();
   await page.getByRole('button', { name: 'Deutsch' }).click();
   await page.getByRole('option', { name: 'Français' }).click();
   await page.getByRole('link', { name: 'Cas' }).click();
+  await page.locator('div:nth-child(3) > .css-1ssirbu > .css-mslw24').click();
   await expect(page.locator('text=Complété')).not.toHaveCount(0);
+  await expect(page.locator('text=Actif')).not.toHaveCount(0);
+  await page.getByRole('link', { name: 'Clients' }).click();
+  await page.locator('div:nth-child(2) > .css-1ssirbu > .css-mslw24 > .MuiButtonBase-root > .PrivateSwitchBase-input').click();
   await expect(page.locator('text=Actif')).not.toHaveCount(0);
   await page.getByRole('button', { name: 'SS', exact: true }).click();
   await page.getByRole('menuitem', { name: 'Paramètres' }).click();
   await page.getByRole('button', { name: 'Français' }).click();
   await page.getByRole('option', { name: 'Italiano' }).click();
   await page.getByRole('link', { name: 'Casi' }).click();
+  await page.locator('div:nth-child(3) > .css-1ssirbu > .css-mslw24').click();
   await expect(page.locator('text=Completato')).not.toHaveCount(0);
+  await expect(page.locator('text=Active')).not.toHaveCount(0);
+  await page.getByRole('link', { name: 'Clienti' }).click();
+  await page.locator('div:nth-child(2) > .css-1ssirbu > .css-mslw24 > .MuiButtonBase-root > .PrivateSwitchBase-input').click();
   await expect(page.locator('text=Active')).not.toHaveCount(0);
 });
