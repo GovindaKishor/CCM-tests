77125d13e746c3a0125bcf39e355aaed4b358b5d
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2023-04-12 17:07:15 +0200

    12/04/2023

---
+++ CCM 6.0 test cases/createconfig.spec.ts
@@ -0,0 +1,27 @@
+//To create a new cofiguration in workbench with more than 100 charecters.
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/');
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/auth');
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/auth/login');
+  await page.getByRole('textbox', { name: 'username' }).fill('SUPERUSER');
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Workbench' }).click();
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/workbench/');
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/workbench/auth');
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/workbench');
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/workbench/configs');
+  await page.locator('button').nth(-1).click();
+  await page.getByLabel('Name *').click();
+  await page.getByLabel('Name *').fill('0123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960616263646566676869707172737475767778798081828384858687888990919293949596979899');
+  await page.getByLabel('Description').click();
+  await page.getByLabel('Description').fill('Example');
+  await page.getByLabel('Duplicate existing configuration').check();
+  await page.getByRole('button', { name: 'Open' }).click();
+  await page.getByRole('listbox', { name: 'Duplicate from' }).getByText('ccm').click();
+  await page.getByRole('button', { name: 'Create' }).click();
+});
