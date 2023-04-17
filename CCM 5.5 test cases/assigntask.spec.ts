28d83edcdf8f7b977e5b2f2045ef0bd106924866
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2023-04-17 13:12:35 +0200

    17/04/2023

---
+++ CCM 5.5 test cases/assigntask.spec.ts
@@ -0,0 +1,19 @@
+//Assign button should be disabled if option is cleared.
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/auth/login');
+  await page.getByRole('textbox', { name: 'username' }).fill('SUPERUSER');
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Tasks' }).click();
+  await page.locator('.css-mslw24').first().click();
+  await page.locator('[data-test-id="virtuoso-top-item-list"]').getByRole('button').first().click();
+  await page.getByRole('menuitem', { name: 'Assign/Unassign' }).click();
+  await page.getByRole('button', { name: 'Open' }).click();
+  await page.getByRole('option', {}).nth(0).click();
+  await page.getByRole('button', { name: 'Clear' }).click();
+  await expect(page.getByRole('button', { name: 'Unassign task' })).toHaveCount(1);
+});
---
+++ CCM 5.5 test cases/editconfiguration.spec.ts
@@ -0,0 +1,27 @@
+//Edit the detials of existing configuration.
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/auth/login');
+  await page.getByRole('textbox', { name: 'username' }).fill('SUPERUSER');
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Workbench' }).click();
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench/');
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench/auth');
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench');
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench/configs');
+  await page.locator('button').nth(-1).click();
+  await page.getByLabel('Name *').click();
+  await page.getByLabel('Name *').fill('Automation Testing');
+  await page.getByLabel('Duplicate existing configuration').check();
+  await page.getByRole('button', { name: 'Open' }).click();
+  await page.getByRole('listbox', { name: 'Duplicate from' }).getByText('ccm').click();
+  await page.getByRole('button', { name: 'Duplicate' }).click();
+  await page.getByRole('menuitem', { name: 'Edit' }).click();
+  await page.getByLabel('Name *').click();
+  await page.getByLabel('Name *').fill('Testing');
+  await page.getByRole('button', { name: 'Edit' }).click();
+});
