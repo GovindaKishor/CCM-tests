5555a890b3bc5161a7c755b29bf45473332eae35
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2023-04-19 16:07:52 +0200

    19/04/2023

---
+++ CCM 5.5 test cases/accountrisk.spec.ts
@@ -0,0 +1,24 @@
+//Exploring Account risk section of risk engine.
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/auth/login');
+  await page.getByRole('textbox', { name: 'username' }).fill('SUPERUSER');
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('link', { name: 'Workbench' }).click();
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench/');
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench/auth');
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench');
+  await page.goto('http://ccm-ci-5-5-x.imtf-devops.com:8181/workbench/configs');
+  await page.getByRole('cell', { name: 'active' }).click();
+  await page.getByRole('button', { name: 'Risk Configurations' }).click();
+  await page.getByRole('link', { name: 'Risk Engines' }).click();
+  await page.getByRole('cell', { name: 'Decision:AccountRisk' }).click();
+  await page.locator('.dmn-icon-decision-table').first().click();
+  await page.getByRole('button', { name: 'View DRD' }).click();
+  await page.locator('div').filter({ hasText: 'Decision:AccountRisk' }).getByRole('button').nth(7).click();
+  await page.getByRole('menuitem', { name: 'Open in Text Editor' }).click();
+});
