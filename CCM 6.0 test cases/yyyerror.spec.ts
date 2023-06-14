3452aec3b6bd4ac3647c58abb4b605679b81ad4b
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2023-06-14 13:55:48 +0200

    YYY error

---
+++ CCM 6.0 test cases/yyyerror.spec.ts
@@ -0,0 +1,32 @@
+//When user changed language to Deustch and Italino YYY,XXX is displayed instead of data.
+
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://ccm-ci-develop.imtf-devops.com:8181/auth/login');
+  await page.getByRole('textbox').nth(0).fill('SUPERUSER');
+  await page.getByRole('textbox').nth(1).fill('SUPERUSER');
+  await page.getByRole('button').nth(2).click();
+  await page.getByRole('button').nth(0).click();
+  await page.getByRole('menuitem').nth(1).click();
+  await page.getByRole('button').nth(3).click();
+  await page.getByRole('option').nth(2).click();
+  await page.getByRole('link').nth(0).click();
+  await expect(page.getByText('YYY')).not.toHaveCount(0);
+  await page.getByRole('link').nth(1).click();
+  await expect(page.getByText('YYY')).not.toHaveCount(0);
+  await page.getByRole('link').nth(2).click();
+  await expect(page.getByText('YYY')).not.toHaveCount(0);
+  await expect(page.getByText('XXX')).not.toHaveCount(0);
+  await page.getByRole('button').nth(0).click();
+  await page.getByRole('menuitem').nth(1).click();
+  await page.getByRole('button').nth(3).click();
+  await page.getByRole('option').nth(4).click();
+  await page.getByRole('link').nth(0).click();
+  await expect(page.getByText('YYY')).not.toHaveCount(0);
+  await page.getByRole('link').nth(1).click();
+  await expect(page.getByText('YYY')).not.toHaveCount(0);
+  await page.getByRole('link').nth(2).click();
+  await expect(page.getByText('YYY')).not.toHaveCount(0);
+  await expect(page.getByText('XXX')).not.toHaveCount(0);
+});
