b7492d3253bd51186eb2851b58d83c8f800f2a5a
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2024-01-24 19:17:56 +0530

    Siron One

---
+++ CCM 6.0 test cases/sironone.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8181/auth/login');
+  await page.getByLabel('username').fill('SUPERUSER');
+  await page.getByLabel('Password *').click();
+  await page.getByLabel('Password *').fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log on' }).click();
+  await page.getByText('Siron®One').click();
+  await expect(page.getByText('Siron®One')).toHaveCount(1);
+});
