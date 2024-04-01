f736d93b4c5444b73eafe71a11d20e0b6c1703c4
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2024-04-01 13:09:34 +0530

    Task buttons

---
+++ CCM 6.0 test cases/taskbuttons.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8181/');
+  await page.goto('http://localhost:8181/auth');
+  await page.goto('http://localhost:8181/auth/login');
+  await page.getByLabel('Username').fill('SUPERUSER');
+  await page.getByLabel('Password', { exact: true }).click();
+  await page.getByLabel('Password', { exact: true }).fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log on' }).click();
+  await page.getByRole('link', { name: 'Tasks' }).click();
+  await page.getByText('HT-5').dblclick();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('button', { name: 'Unusual-Activity Report' }).click();
+});
