33057ae022a161b7330a8dded555c2bb0596d82f
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2024-04-04 14:12:35 +0530

    Siron One at bottom

---
+++ CCM 6.0 test cases/sirononeatbottom.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8181/');
+  await page.goto('http://localhost:8181/auth');
+  await page.goto('http://localhost:8181/auth/login');
+  await page.getByLabel('Username').fill('SUPERUSER');
+  await page.getByLabel('Password', { exact: true }).click();
+  await page.getByLabel('Password', { exact: true }).fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log on' }).click();
+  await page.getByText('Siron®One').click();
+});
