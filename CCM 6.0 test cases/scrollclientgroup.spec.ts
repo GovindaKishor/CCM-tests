1efa34240cc30fe6510bf407b99cb17ed098cc20
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2024-04-11 19:21:12 +0530

    Scroll Client Group

---
+++ CCM 6.0 test cases/scrollclientgroup.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8181/');
+  await page.goto('http://localhost:8181/auth');
+  await page.goto('http://localhost:8181/auth/login');
+  await page.getByLabel('Username').fill('SUPERUSER');
+  await page.getByLabel('Password', { exact: true }).click();
+  await page.getByLabel('Password', { exact: true }).fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log on' }).click();
+  await page.getByRole('link', { name: 'Client 360' }).click();
+  await page.getByText('testing - CEMT').dblclick();
+  await page.mouse.move(0, 1000);
+});
