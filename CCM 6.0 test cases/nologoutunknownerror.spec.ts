fd7dddca02813aec1b7fb3f6b6f225e2f751ba57
GovindaKishor <117365949+GovindaKishor@users.noreply.github.com>
2024-05-14 17:40:14 +0530

    No logout unknown error

---
+++ CCM 6.0 test cases/nologoutunknownerror.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:8181/');
+  await page.goto('http://localhost:8181/auth');
+  await page.goto('http://localhost:8181/auth/login');
+  await page.getByLabel('Username').fill('SUPERUSER');
+  await page.getByLabel('Password', { exact: true }).click();
+  await page.getByLabel('Password', { exact: true }).fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log on' }).click();
+  await page.getByRole('banner').getByRole('button').nth(3).click();
+  await page.getByRole('menuitem', { name: 'Logoff' }).click();
+  await page.getByRole('link', { name: 'Log on again' }).click();
+  await page.getByLabel('Username').fill('SUPERUSER');
+  await page.getByLabel('Password', { exact: true }).click();
+  await page.getByLabel('Password', { exact: true }).fill('SUPERUSER');
+  await page.getByRole('button', { name: 'Log on' }).click();
+  await expect(page.getByText('Unknown error')).toHaveCount(0);
+});
